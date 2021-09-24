57f375243bf706f76afa0d0adc5cd51e04d2b871
OlhaNester <olga_nv@ukr.net>
2021-09-24 16:39:15 +0300

    444

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,41 +1,56 @@
 import { Component } from "react";
+import shortid from 'shortid';
 
 class Form extends Component {
     state = {
         name: '',
         number: ''
     }
+    nameImputId = shortid.generate();
+    numberImputId = shortid.generate();
+
      handleChange = (event) => {
     const { name, value } = event.currentTarget;
     this.setState({[name]: value})
     }
      handleSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state);
-  };
+        
+         this.props.onSubmit(this.state);
+         this.reset();
+    };
+    
+    reset = () => {
+        this.setState({
+            name: '',
+            number: ''
+        });
+    }
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <label>Name
+                <label htmlFor={this.nameImputId}>Name
                     <input
                         type="text"
                         name="name"
                         value={this.state.name} onChange={this.handleChange}
+                        id={this.nameImputId}
                         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                         title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
                         required
                     />
                 </label>
-                <label>Phone Number
+                <label htmlFor={this.numberImputId}>Phone Number
                     <input
                         type="tel"
                         name="number" value={this.state.number} onChange={this.handleChange}
+                        id={this.numberImputId}
                         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                         title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
                         required
                     />
                 </label>
-                <button type="submit">Add contact</button>);
+                <button type="submit">Add contact</button>
             </form>
       
         )
