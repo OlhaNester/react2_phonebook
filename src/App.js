62e78a8860515fd207b18166d4557479102f2ad5
OlhaNester <olga_nv@ukr.net>
2021-09-24 12:14:19 +0300

    333

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import { Component } from "react";
 import "./App.css";
+import Form from './components/Form';
 
 class App extends Component {
+  state = {
+    contacts: [],
+    
+  }
+
+  formHandleSubmit = (data) => {
+    console.log(data);
+  }
+ 
+  
+ 
+
   render() {
-    return <div className="App"></div>;
+    return <div className="App">
+      <Form onSubmit={ this.formHandleSubmit}/>
+      
+    </div>;
   }
 }
 
