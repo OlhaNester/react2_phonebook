cfa2b2278fbf222ab92e265bf209a75f2c7dc644
OlhaNester <olga_nv@ukr.net>
2021-10-02 20:34:43 +0300

    222

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ import shortid from "shortid";
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
   };
 
@@ -65,7 +60,10 @@ class App extends Component {
   componentDidMount() {
     const contacts = localStorage.getItem("contacts");
     const parseContacts = JSON.parse(contacts);
-    this.setState({ contacts: parseContacts });
+
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts });
+    }
   }
   componentDidUpdate(prevProps, prevState) {
     if (this.state.contacts !== prevState.contacts) {
